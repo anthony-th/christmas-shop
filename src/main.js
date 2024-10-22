26cffe8714c819214e9b494cb6354625bfb6aa77
anthony-th <yefremov.anthony@gmail.com>
2024-10-22 21:55:04 +0300

    feat: change vite.config, add basic scss styles for components orientation

---
+++ src/main.js
@@ -0,0 +1,5 @@
+// import { header } from './components/header';
+// import { main } from './components/main';
+// import { footer } from './components/footer';
+
+// document.body.append(header, main, footer);
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,18 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
+  base: '/christmas-shop/',
   build: {
     sourcemap: true,
   },
-  base: '/christmas-shop/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src")}],
+  },
 });
