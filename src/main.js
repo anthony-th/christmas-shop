0ca057fc6ab6897bccf0350b16c87a550807f26f
anthony-th <yefremov.anthony@gmail.com>
2024-10-26 21:07:43 +0300

    feat: add fonts, a little styles and code header

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
-import { header } from './components/header/Header';
+import './assets/styles/_typography.scss';
+import './assets/styles/_global.scss';
+import './assets/styles/_media_hover.scss';
+
+import { header } from '@/components/header/Header';
 
 document.body.append(header);
