98657267a2c198a625e9d5620fd973bd6c1c8736
anthony-th <yefremov.anthony@gmail.com>
2024-10-27 11:45:48 +0300

    feat: start heroSection, add some styles

--- src/components/layouts/main/Main.js
+++ src/components/layouts/main/Main.js
@@ -1,6 +1,8 @@
 import './Main.scss';
 import { createElement } from '../../common/createElement';
+import { heroSection } from '../../sections/hero/Hero';
 
 const main = createElement('main', ['main']);
 
+main.append(heroSection);
 export { main };
---
+++ src/components/sections/hero/Hero.js
@@ -0,0 +1,6 @@
+import './Hero.scss';
+import { createElement } from '../../common/createElement';
+
+const heroSection = createElement('section', ['section', 'hero']);
+
+export { heroSection };
