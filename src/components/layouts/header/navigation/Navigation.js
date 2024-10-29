da3247d1a1036672b0ec9cb1d0beafc9d116c963
anthony-th <yefremov.anthony@gmail.com>
2024-10-29 20:46:05 +0300

    feat: add active link for giftsPage

--- src/components/layouts/header/navigation/Navigation.js
+++ src/components/layouts/header/navigation/Navigation.js
@@ -10,6 +10,9 @@ const links = [
 const createNav = () => {
   return links.map((link) => {
     const anchor = createElement('a', ['header__nav-link', 'user-select-none']);
+    if (link.url === window.location.pathname) {
+      anchor.classList.add('link-active');
+    }
     anchor.textContent = link.textContent;
     anchor.href = link.url;
     return anchor;
