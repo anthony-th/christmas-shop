dc7cecfb6df7ec40ff40109bc2d00f8152d02c30
anthony-th <yefremov.anthony@gmail.com>
2024-11-01 01:06:09 +0300

    fix: background about fix, del trash styles and img

--- src/components/sections/about/About.js
+++ src/components/sections/about/About.js
@@ -9,10 +9,8 @@ const aboutCaption = createElement('h3', ['about__caption'], 'About');
 const aboutTitle = createElement('h2', ['about__title'], 'Unleash your inner superhero!');
 const aboutParagraph = createElement('p', ['about__paragraph'], 'This New Year marks the beginning of your journey to inner harmony and new strengths. We offer unique gifts that will help you improve your life.');
 const imgContainer = createElement('div', ['about__img-container']);
-const imageSanta = createElement('img', ['about__img'], '', { src: '../../assets/images/santa.webp', alt: 'Santa' });
 
 textBlock.append(aboutCaption, aboutTitle, aboutParagraph);
-imgContainer.append(imageSanta);
 textContainer.append(textBlock);
 aboutContainer.append(textContainer, imgContainer);
 aboutSection.append(aboutContainer);
