0c8b01412a67db236d0bcc33307f874a9e2be530
anthony-th <yefremov.anthony@gmail.com>
2024-11-02 17:16:12 +0300

    feat: add some elements to bestSection

--- src/components/sections/best/Best.js
+++ src/components/sections/best/Best.js
@@ -2,5 +2,13 @@ import './Best.scss';
 import { createElement } from '@com/common/createElement';
 
 const bestSection = createElement('section', ['section', 'best'],'', { id: 'best' });
+const bestContainer = createElement('div', ['best__container']);
+const bestTextContainer = createElement('div', ['best__text-container']);
+const bestCaption = createElement('h3', ['best__caption'], 'Best Gifts');
+const bestTitle = createElement('h2', ['best__title'], 'especially for you');
+const cardsContainer = createElement('div', ['best__cards-container']);
 
+bestTextContainer.append(bestCaption, bestTitle);
+bestContainer.append(bestTextContainer, cardsContainer);
+bestSection.append(bestContainer);
 export { bestSection };
