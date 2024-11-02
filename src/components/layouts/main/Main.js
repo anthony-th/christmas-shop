b73978378a2d44c829639d0f6911674322b0c588
anthony-th <yefremov.anthony@gmail.com>
2024-11-02 17:03:36 +0300

    feat: add base bestSection

--- src/components/layouts/main/Main.js
+++ src/components/layouts/main/Main.js
@@ -3,8 +3,9 @@ import { createElement } from '@com/common/createElement';
 import { heroSection } from '@com/sections/hero/Hero';
 import { aboutSection } from '@com/sections/about/About';
 import { sliderSection } from '@com/sections/slider/Slider';
+import { bestSection } from '@com/sections/best/Best';
 
 const main = createElement('main', ['main']);
 
-main.append(heroSection, aboutSection, sliderSection);
+main.append(heroSection, aboutSection, sliderSection, bestSection);
 export { main };
---
+++ src/components/sections/best/Best.js
@@ -0,0 +1,6 @@
+import './Best.scss';
+import { createElement } from '@com/common/createElement';
+
+const bestSection = createElement('section', ['section', 'best'],'', { id: 'best' });
+
+export { bestSection };
