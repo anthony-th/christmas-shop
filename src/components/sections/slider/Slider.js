35950cd76d08b74700dfdf483abc03e1766f536a
anthony-th <yefremov.anthony@gmail.com>
2024-11-02 16:55:40 +0300

    feat: complete layout sliderSection

--- src/components/sections/slider/Slider.js
+++ src/components/sections/slider/Slider.js
@@ -6,21 +6,57 @@ const sliderContainer = createElement('div', ['slider__container']);
 const sliderTextContainer = createElement('div', ['slider__text-container']);
 const sliderCaption = createElement('h3', ['slider__caption'], 'Become Happier!');
 const sliderTitle = createElement('h2', ['slider__title'], 'in the new 2025');
-const sliders = createElement('div', ['slider__sliders']);
+const sliders = createElement('div', ['sliders']);
 const sliderNavigation = createElement('div', ['slider__navigation']);
-const leftSliderBtn = createElement('button', ['slider__button'], '', { disabled: true });
-const leftIcon = createElement('svg', ['slider__icon', 'inactive-arrow'], `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-  <path d="M18.5 12H6M6 12L12 6M6 12L12 18" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`, {}, true);
-const rightSliderBtn = createElement('button', ['slider__button', 'cursor-pointer']);
-const rightIcon = createElement('svg', ['slider__icon'], `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-  <path d="M6 12H18.5M18.5 12L12.5 6M18.5 12L12.5 18" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`, {}, true);
+
+const createButtons = (classes, iconClasses, iconSvg, isDisabled = false) => {
+  const buttonOptions = isDisabled ? { disabled: true } : {};
+  const button = createElement('button', classes, '', buttonOptions);
+  const icon = createElement('svg', iconClasses, iconSvg, {}, true);
+  button.append(icon);
+  return button;
+};
+
+const leftBtn = createButtons (
+  ['slider__button'],
+  ['slider__icon', 'inactive-arrow'],
+  `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+     <path d="M18.5 12H6M6 12L12 6M6 12L12 18" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+   </svg>`,
+  true
+);
+
+const rightBtn = createButtons (
+  ['slider__button', 'cursor-pointer'],
+  ['slider__icon'],
+  `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+     <path d="M6 12H18.5M18.5 12L12.5 6M18.5 12L12.5 18" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+   </svg>`
+);
+
+const texts = ['Live', 'create', 'Love', 'dream'];
+
+const imageSrc = [
+  '../../../assets/images/snowman.webp',
+  '../../../assets/images/christmas-trees.webp',
+  '../../../assets/images/christmas-tree-ball.webp',
+  '../../../assets/images/fairytale-house.webp'
+];
+
+const createItems = (texts, imageSrc) => {
+  texts.forEach((text, index) => {
+    const textElement = createElement('div', ['slider__text'], text);
+    const imageContainer = createElement('div', ['slider__image-container']);
+    const image = createElement('img', ['slider__image'], '', { src: imageSrc[index], alt: 'slider image' });
+    imageContainer.append(image);
+    sliders.append(textElement, imageContainer);
+  });
+};
+
+createItems(texts, imageSrc);
 
 sliderTextContainer.append(sliderCaption, sliderTitle);
-leftSliderBtn.append(leftIcon);
-rightSliderBtn.append(rightIcon);
-sliderNavigation.append(leftSliderBtn, rightSliderBtn);
+sliderNavigation.append(leftBtn, rightBtn);
 sliderContainer.append(sliderTextContainer, sliders, sliderNavigation);
 sliderSection.append(sliderContainer);
 export { sliderSection };
