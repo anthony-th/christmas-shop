a2eba5ed095512114fbb3319423f931f703a1f51
anthony-th <yefremov.anthony@gmail.com>
2024-11-03 21:32:37 +0300

    feat: footer cleanup on 404page

--- src/components/views/404/404.js
+++ src/components/views/404/404.js
@@ -3,10 +3,12 @@ import '@scss/_global.scss';
 import './404.scss';
 import { createElement } from '@com/common/createElement';
 import { main } from '@lay/main/Main';
+import { footer } from '@lay/footer/Footer';
 
 const page404 = () => {
   document.title = "404";
   main.innerHTML = '';
+  footer.innerHTML = '';
   const title = createElement('h2', ['page-404__title'], '404');
   const subTitle = createElement('h3', ['page-404__subtitle'], 'Page not found');
   const homeLink = createElement('a', ['page-404__link', 'cursor-pointer'], 'HOME', { href: '/' });
