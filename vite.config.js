2e04fc219f9b32053afbbfe20d04a638f2c9107d
anthony-th <yefremov.anthony@gmail.com>
2024-11-19 01:58:01 +0300

    refactor: add devSourcemap for css \ vite.config

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig(({ command }) => {
       },
     },
     css: {
+      devSourcemap: true,
       preprocessorOptions: {
         scss: {
           api: 'modern-compiler',
