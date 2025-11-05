59a23aafcf8e5fb247fad8fc068e896796d9d363
anthony-th <yefremov.anthony@gmail.com>
2025-11-05 08:27:02 +0300

    style: correct stylelint styles

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,8 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ["stylelint-config-standard-scss"]
+  extends: ["stylelint-config-standard-scss"],
+  rules: {
+    "alpha-value-notation": "number",
+    "selector-class-pattern": null
+  }
 };
